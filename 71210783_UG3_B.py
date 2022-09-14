e39d8312613b995623db9938263e7aeb74658edc
lianachienti <93342573+lianachienti@users.noreply.github.com>
2022-09-14 19:06:26 +0700

    Add files via upload

---
+++ 71210783_UG3_B.py
@@ -0,0 +1,16 @@
+#71210783
+#LianaC
+#UG3
+
+n = int(input("Masukan range data: "))
+dc = dict()
+
+for x in range(n):
+    if x % 2 ==0:
+        n = x
+        fact = 1
+
+
+        
+        dc[x] = fact
+print(dc)
